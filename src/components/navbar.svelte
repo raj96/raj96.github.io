e11ab36424df1fddb98e59d69f25152d520d9f5a
raj96 <rajpeaks@gmail.com>
2022-08-14 18:22:18 +0000

    Adding a trivial navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    let active_el: HTMLElement
+    
+    const ToggleActiveEl = (ev: Event) => {
+        if(active_el !=  undefined) {
+            active_el.classList.toggle("selected")
+        }
+        active_el = ev.target
+        active_el.classList.toggle("selected")
+    }
+
+</script>
+
+<nav class="navbar">
+	<div class="nav-logo">
+        RP
+    </div>
+	<div class="nav-content">
+		<div on:click={ToggleActiveEl} class="nav-item"><a href="#home">Home</a></div>
+		<div class="nav-item"><a href="#projects">Projects</a></div>
+		<div class="nav-item"><a href="#intresets">Intresets</a></div>
+		<div class="nav-item"><a href="#melikey">Contact Me</a></div>
+	</div>
+</nav>
+
+<style>
+    @tailwind components;
+
+    .navbar {
+        @apply flex bg-yellow-400;
+    }
+
+    .nav-logo {
+        @apply text-4xl pl-2;
+    }
+
+    .nav-content {
+        @apply w-full flex justify-evenly items-center transition-all;
+    }
+
+    .nav-content + a {
+        @apply text-lg;
+    }
+
+    .selected {
+        @apply border-b-4 border-b-black;
+    }
+
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,7 @@
 <script>
-    import "../app.css"
+	import Navbar from '../components/navbar.svelte';
+	import '../app.css';
 </script>
 
+<Navbar />
 <slot />
